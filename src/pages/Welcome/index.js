7740f7167d7b0d89e4f15cbe847b4789348eda08
DrigsGomes <rodrigo.vitor169@gmail.com>
2022-03-17 22:51:23 -0300

    Update Welcome

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -11,14 +11,22 @@ export default function Welcome() {
  return (
    <View style={styles.container}>
 
-     <View>
+     <View style={styles.containerLogo}>
        <Image
        source={require('../../assets/logo.png')}
        style={{ width: '100%'}}
        resizeMode='contain'
-
-       
        />
+     </View>
+
+     <View style={styles.containerForm}>
+       <Text style={styles.title}> Monitore e organize seus gastos de qualquer lugar!</Text>
+       <Text style={styles.text}> Faça o login para começar</Text>
+
+       <TouchableOpacity style={styles.button}>
+         <Text style={styles.buttonText}> Acessar</Text>
+       </TouchableOpacity>
+
      </View>
 
    </View>
@@ -29,7 +37,54 @@ export default function Welcome() {
 const styles = StyleSheet.create({
 
   container:{
+    flex:1,
+    backgroundColor:'#38A69D'
+  },
 
+  containerLogo:{
+    flex:2,
+    backgroundColor: '#38A69D',
+    justifyContent:'center',
+    alignItems:'center'
+  
+  },
+
+  containerForm:{
+    flex:1,
+    backgroundColor: '#FFF',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingStart: '5%',
+    paddingEnd: '5%'
+  },
+
+  title:{
+    fontSize:24,
+    fontWeight: 'bold',
+    marginTop:28,
+    marginBottom:12
+  },
+
+  text:{
+    color:'#A1A1A1'
+  },
+
+  button:{
+    position: 'absolute',
+    backgroundColor: '#38A69D',
+    borderRadius: 50,
+    paddingVertical:8,
+    width: '60%',
+    alignSelf:'center',
+    bottom:'15%',
+    alignItems:'center',
+    justifyContent:'center'
+  },
+
+  buttonText:{
+    fontSize:18,
+    color:'#FFF',
+    fontWeight:'bold'
   }
 
 })
